421c6cf2c148c36c0e2a9fbc4d2c1eb095815951
BryanPool99 <bryanpool991@hotmail.com>
2026-01-15 19:21:35 -0500

    reto de qr code componente terminado

---
+++ qr-code-componente-main/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  title = 'Improve your front-end skills by building projects';
+  description =
+    'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level';
+}
